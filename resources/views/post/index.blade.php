b59927ac6cee19cce554bbed51258e525bc03dca
Your Name <you@example.com>
2022-12-12 14:44:16 +0200

    кроп фото

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,7 +9,7 @@
             <div class="left">
                 <img loading="lazy"
                      title="Проститутка {{ $post->name }}"
-                     srcset="/211-300/thumbs/{{$post->avatar}}" alt="{{ $post->name }}">
+                     srcset="/600-700/thumbs/{{$post->avatar}}" alt="{{ $post->name }}">
             </div>
             <div class="right">
                 <h1>{{ $post->name }}</h1>
