733097bd1eddb6c976a96443f315bd9cb0692fff
Your Name <you@example.com>
2023-02-08 13:48:35 +0200

    импорт

--- resources/views/intim-box/post/index.blade.php
+++ resources/views/intim-box/post/index.blade.php
@@ -100,7 +100,7 @@
                             <div class="profile-main-info__video-text">
                                 Видео
                             </div>
-                            <a data-fancybox="video" href="{{ $post->video }}"
+                            <a data-fancybox="video" href="/storage/{{ $post->video }}"
                                class="profile-main-info__video-bg">
                                 <img class="profile-main-info__video-img"
                                      src="/intim-box/images/content/profile/profile-photo.jpg" alt="">
