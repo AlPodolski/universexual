f54e1c9bc7dc4091466ab5430b54ca9b374cbd74
Your Name <you@example.com>
2023-02-15 16:31:41 +0200

    правки

--- app/Repository/PostRepository.php
+++ app/Repository/PostRepository.php
@@ -164,7 +164,7 @@ class PostRepository
         if ($salon) $posts = $posts->where('type', Post::SALON_TYPE);
         if ($indi) $posts = $posts->where('type', Post::INDI_TYPE);
 
-        if (count($posts->getQuery()->wheres) <= 2) abort(404);
+        if (count($posts->getQuery()->wheres) <= 2 and !$posts->getQuery()->orders) abort(404);
 
         $posts = $posts
             ->with('place', 'reviews', 'city')
