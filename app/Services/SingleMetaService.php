1e1719d7456a853b8b48945203caac12e0426d6b
Your Name <you@example.com>
2023-02-20 10:20:01 +0200

    мета теги для деталки второго сайта

--- app/Services/SingleMetaService.php
+++ app/Services/SingleMetaService.php
@@ -8,10 +8,45 @@ class SingleMetaService
 {
     public function makeMetaTags(Post $post, $cityInfo): array
     {
-        $data['title'] = $this->makeTitle($post, $cityInfo);
-        $data['des'] = $this->makeDes($post, $cityInfo);
+
+        if (SITE_ID == 1){
+            $data['title'] = $this->makeTitle($post, $cityInfo);
+            $data['des'] = $this->makeDes($post, $cityInfo);
+        }
+        elseif (SITE_ID == 2){
+            $data['title'] = $this->makeTitleForIntimBox($post, $cityInfo);
+            $data['des'] = $this->makeDesForIntimBox($post, $cityInfo);
+        }
 
         return $data;
+    }
+
+    private function makeTitleForIntimBox(Post $post, $cityInfo): string
+    {
+
+        $title = 'Проститутка '.$post->name . ' ID '.$post->id;
+
+        if ($post->breast) $title .= ', грудь '.$post->breast;
+        if ($post->ves) $title .= ', вес '.$post->ves;
+        if ($post->rost) $title .= ', рост '.$post->rost;
+
+        if ($post->price) $title .= ' – от '.$post->price .' руб. ';
+
+        $title .= ' '.$cityInfo['city3'] ;
+
+        return $title;
+
+    }
+
+    private function makeDesForIntimBox(Post $post, $cityInfo): string
+    {
+
+        $des = 'Анкета проститутки '.$post->name . ' '.$post->id . ' с фото, ценами и параметрами. Если понравилась и хочешь снять – звони ей по номеру ';
+
+        $des .= $post->phone;
+
+        return $des;
+
     }
 
     private function makeTitle(Post $post, $cityInfo): string
