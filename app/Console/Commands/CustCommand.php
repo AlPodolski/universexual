0b99bfa5254644e64f302b85c8b978d60e5c59f7
Precision <sanyapodolski@gmail.com>
2023-04-07 10:04:42 +0300

    edit

--- app/Console/Commands/CustCommand.php
+++ app/Console/Commands/CustCommand.php
@@ -22,7 +22,7 @@ class CustCommand extends Command
 
         foreach ($posts as $post){
 
-            if (!$post->apartament_2_hour_price) $post->apartament_2_hour_price = $post->price * 2;
+            $post->apartament_2_hour_price = $post->price * 2;
             if (!$post->apartament_night_price) $post->apartament_night_price = $post->price * 5;
             if (!$post->exit_1_hour_price) $post->exit_1_hour_price = $post->price + 1000;
             if (!$post->exit_2_hour_price) $post->exit_2_hour_price = $post->exit_1_hour_price * 2;
