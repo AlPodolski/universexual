2c08687e36cd31c5b68cf8b199bff54976038c8e
Precision <sanyapodolski@gmail.com>
2023-08-22 16:52:04 +0300

    правки

--- app/Repository/PostRepository.php
+++ app/Repository/PostRepository.php
@@ -22,7 +22,7 @@ class PostRepository
     {
         $posts = Post::where('city_id', $cityId)
             ->where(['site_id' => SITE_ID, 'publication_status' => Post::POST_ON_PUBLICATION])
-            ->with('metro', 'reviews', 'city')
+            ->with('metro', 'reviews', 'city', 'place')
             ->orderByRaw($this->sort)
             ->paginate(20);
 
