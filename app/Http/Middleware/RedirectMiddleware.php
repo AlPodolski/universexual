4763bb7dfe9547c8325cd343f4e985497fa2a2d6
Precision <sanyapodolski@gmail.com>
2023-08-31 09:45:02 +0300

    правки

--- app/Http/Middleware/RedirectMiddleware.php
+++ app/Http/Middleware/RedirectMiddleware.php
@@ -21,7 +21,7 @@ class RedirectMiddleware
 
             $redirect = Cache::remember('redirect_' . $actualCity.'_'.SITE, $expire, function () use ($actualCity) {
 
-                $redirect = Redirect::where('from', $actualCity)->first();
+                $redirect = Redirect::where('from', $actualCity)->where('site', SITE_ID)->first();
 
                 if ($redirect) return $redirect;
 
