bcab7ff4792e124f89b30060acbbb3e65386768f
Precision <sanyapodolski@gmail.com>
2023-09-08 16:25:02 +0300

    аякс загрузка карточек

--- resources/views/intim-box/layouts/main.blade.php
+++ resources/views/intim-box/layouts/main.blade.php
@@ -34,7 +34,7 @@
         @yield('open-graph')
     @endif
 
-    <link rel="stylesheet" href="/intim-box/css/style.css?v=14">
+    <link rel="stylesheet" href="/intim-box/css/style.css?v=15ё">
 </head>
 
 <body>
