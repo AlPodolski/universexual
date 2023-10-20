c96eac55f2ad8b01f8a5343c67738530c8ec5d12
Precision <sanyapodolski@gmail.com>
2023-10-20 10:49:51 +0300

    вывод оплат пользователей

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="/css/admin.css?v=6" rel="stylesheet">
+    <link href="/css/admin.css?v=7" rel="stylesheet">
     <link href="/intim-box/css/cabinet.css?v=5" rel="stylesheet">
 
 </head>
