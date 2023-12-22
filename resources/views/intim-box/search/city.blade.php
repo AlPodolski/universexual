d8a5954f59cf67475b7d13cdf11b57d15a321dfd
Precision <sanyapodolski@gmail.com>
2023-12-22 17:27:10 +0200

    сайт мап

--- resources/views/intim-box/search/city.blade.php
+++ resources/views/intim-box/search/city.blade.php
@@ -1,7 +1,7 @@
 @if($cityList->first)
     @foreach($cityList as $item)
         <li class="header__location-list__sub-item">
-            <a href="https://{{ $item->url }}{{ SITE }}" class="header__location-list__sub-link link-reset">
+            <a href="https://{{ $item->url }}.{{ SITE }}" class="header__location-list__sub-link link-reset">
                 {{ $item->city }}
             </a>
         </li>
