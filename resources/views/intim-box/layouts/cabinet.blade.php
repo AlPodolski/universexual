05f324b64614669615c5a5db048e7fe202d26241
Dell <alpodolski11@gmail.com>
2024-04-09 15:30:11 +0300

    снятие с публикации все анкет

--- resources/views/intim-box/layouts/cabinet.blade.php
+++ resources/views/intim-box/layouts/cabinet.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="/intim-box/css/cabinet.css?v=28">
+    <link rel="stylesheet" href="/intim-box/css/cabinet.css?v=29">
 </head>
 
 <body>
@@ -64,7 +64,7 @@
     </div>
 
 </div>
-<script defer src="/intim-box/js/cabinet.js?v=27"></script>
+<script defer src="/intim-box/js/cabinet.js?v=28"></script>
 </body>
 
 </html>
