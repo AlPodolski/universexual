f92e9c5d1df26d718393f4878556cfa529789730
aleksandrpodolskij <sanyapodolski@gmail.com>
2025-04-15 17:09:21 +0300

    sorting

--- app/Actions/GetSort.php
+++ app/Actions/GetSort.php
@@ -4,8 +4,7 @@ namespace App\Actions;
 
 class GetSort
 {
-    private string $default = 'tarif_id DESC, fake DESC, RAND()';
-
+    private string $default = 'tarif_id DESC, fake DESC, sorting DESC';
     private string $priceDesc = 'price DESC';
     private string $priceAsc = 'price ASC';
 
